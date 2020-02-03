fc2b35ec292861eabfc492ef5024c339590e8f13
Phu Chau <vinhphu101195@gmail.com>
2020-02-03 19:59:53 +0200

    move json file

--- src/RestaurantsProvider/index.js
+++ src/RestaurantsProvider/index.js
@@ -1,5 +1,5 @@
 import React, { createContext,useContext } from "react";
-import file from "../data/restaurants.json";
+import file from "./restaurants.json";
 
 export const DataContext = createContext();
 
